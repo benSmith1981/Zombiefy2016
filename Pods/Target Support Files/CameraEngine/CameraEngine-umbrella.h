33ee9d710198d9de451061e9a1d3dab689f86461
Ben Smith <benjaminsmith1981@gmail.com>
2016-11-12 21:51:12 +0100

    First commit

---
+++ Pods/Target Support Files/CameraEngine/CameraEngine-umbrella.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+#import "CameraEngine.h"
+#import "VideoEncoder.h"
+
+FOUNDATION_EXPORT double CameraEngineVersionNumber;
+FOUNDATION_EXPORT const unsigned char CameraEngineVersionString[];
+
