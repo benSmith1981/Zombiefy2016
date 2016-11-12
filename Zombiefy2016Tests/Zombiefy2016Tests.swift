242f410350e7bed8e7e83e6083b834821cb26eac
Ben Smith <benjaminsmith1981@gmail.com>
2016-11-12 19:13:59 +0100

    Initial Commit

---
+++ Zombiefy2016Tests/Zombiefy2016Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Zombiefy2016Tests.swift
+//  Zombiefy2016Tests
+//
+//  Created by Ben Smith on 12/11/16.
+//  Copyright © 2016 Ben Smith. All rights reserved.
+//
+
+import XCTest
+@testable import Zombiefy2016
+
+class Zombiefy2016Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
